4039ae454bb4279e21083ca516834fabd831126d
hyoseon.shin <cctech00@gmail.com>
2017-05-03 23:14:46 +0900

    Initial Commit

---
+++ src/main/java/com/example/dao/Board.java
@@ -0,0 +1,42 @@
+package com.example.dao;
+
+public class Board {
+
+	private int seq;
+	private String title;
+	private String content;
+	private String writer;
+	private int parentSeq;
+
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getParentSeq() {
+		return parentSeq;
+	}
+	public void setParentSeq(int parentSeq) {
+		this.parentSeq = parentSeq;
+	}
+
+}
